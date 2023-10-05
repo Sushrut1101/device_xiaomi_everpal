3c28b4ebbff3c5e9eb9b069e62f7f7992a02e50f
Sushrut1101 <guptasushrut@gmail.com>
2023-10-05 12:17:15 +0530

    everpal: AOSP Bringup
    Change-Id: Ia16dc6a59c223143535431ad5fa01af2ad3b81d3
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_everpal.mk
+    $(LOCAL_DIR)/everpal.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_everpal-user \
-    lineage_everpal-userdebug \
-    lineage_everpal-eng
+    everpal-user \
+    everpal-userdebug \
+    everpal-eng
--- everpal.mk
+++ everpal.mk
@@ -8,14 +8,11 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
 # Inherit from everpal device
 $(call inherit-product, device/xiaomi/everpal/device.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_everpal
+PRODUCT_NAME := everpal
 PRODUCT_DEVICE := everpal
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
